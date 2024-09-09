ecf9cffdc8a0976fa8d1a3a79a3d66ca47de7fa2
Trevor Pawlewicz <trev@Trevors-MBP.lan>
2024-09-09 11:35:28 -0700

    Drexel MSSE update

--- src/data/educationdata.js
+++ src/data/educationdata.js
@@ -3,7 +3,7 @@ const educationdata = [
       title: "Drexel University Master's Degree",
       image: "./images/DrexelUniversity-Logo.svg",
       description:
-        "Masters of Science in Software Enginerring (Graduating in Summer 2024)",
+        "Masters of Science in Software Engineering",
     },
     {
       title: "General Assembly Santa Monica Cohort",
